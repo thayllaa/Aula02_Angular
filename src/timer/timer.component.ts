9d8ebfbf9dd3ee9751080396959c7ca3a1575fe9
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 20:55:15 -0300

    1 file modified

--- src/timer/timer.component.ts
+++ src/timer/timer.component.ts
@@ -2,15 +2,39 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
-  standalone: true,
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
+  timeLeft: number = 0;
+  interval: any;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  start(): void {
+    this.interval = setInterval(() => {
+      this.timeLeft--;
+      if (this.timeLeft === 0) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  pause(): void {
+    clearInterval(this.interval);
+  }
+
+  reset(): void {
+    clearInterval(this.interval);
+    this.timeLeft = 0;
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 }
