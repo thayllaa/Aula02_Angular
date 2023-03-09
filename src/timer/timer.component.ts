90ba46cbd946c10fa53804f4df0a6357115f2d9c
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 21:14:32 -0300

    1 file modified

--- src/timer/timer.component.ts
+++ src/timer/timer.component.ts
@@ -7,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
+  counter: number = 0;
 
-  constructor() { }
+  constructor() {
+    timer = setInterval(() => {
+      this.counter = this.counter + 1;
+    }, 1000);
+  }
+
+  getSecs() {
+    return Math.trunc(this.counter % 60);
+  }
+
+  getMin() {
+    return Math.trunc(this.counter/60);
+  }
 
   ngOnInit() {
   }
