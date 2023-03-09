c48d3068122b36cff4661a4d64248986cc18b7ad
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 21:42:50 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { MeComponent } from './me/me.component';
 import { TimerComponent } from './timer/timer.component';
+import { NumberComponent } from './number/number.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, MeComponent, TimerComponent],
+  imports: [CommonModule, MeComponent, TimerComponent, NumberComponent],
   template: `
     <h1> {{name}} </h1>
     <app-me></app-me>
     <app-timer></app-timer>
+    <app-number></app-number>
   `,
 })
 export class App {
