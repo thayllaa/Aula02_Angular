515c8356217341b5d5854e97249ac7764c0b5478
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 21:55:35 -0300

    1 file modified

--- src/number/number.component.ts
+++ src/number/number.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-number',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './number.component.html',
   styleUrls: ['./number.component.css']
 })
 export class NumberComponent implements OnInit {
+  constructor() {}
 
   getNumbers() {
     return [21, 65, 2, 0, 78, 11, 77, 122, 907, 1]
