fbf6506728f27be05bb3f9c8ff1e1bef79b89043
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 21:39:52 -0300

    2 files added

---
+++ src/number/number.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number',
+  standalone: true,
+  templateUrl: './number.component.html',
+  styleUrls: ['./number.component.css']
+})
+export class NumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
