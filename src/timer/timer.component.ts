bffcc27068e0ddcd7ab4efcac7d678f980af931e
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 21:07:00 -0300

    3 files added

---
+++ src/timer/timer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
